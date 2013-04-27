5ed84991b84f8c78649edb98171e4b1f9159fde7
uztnus <uztnus@gmail.com>
2013-04-27 14:02:57 +0300

    added morse rule

--- morsejs/player.js
+++ morsejs/player.js
@@ -22,3 +22,8 @@ MorsePlayer.prototype.stop = function(){
 	
 };
 
+//The rules of 1/3 and 1/2/4:
+//Morse code is: tone for one unit (dit) or three units (dah)
+//followed by the sum of one unit of silence (always),
+//plus two units of silence (if end of letter),
+//plus four units of silence (if also end of word).
